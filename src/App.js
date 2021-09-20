d8c418033e3e838946c335efe402fa1123d03219
Kristian Fulkerson <kristianf451@gmail.com>
2021-09-20 11:20:36 -0700

    added axiosWithAuth util, made Header Component for nav, started PotluckList

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Header from "./components/Header";
+import PotluckList from "./components/PotluckList";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/potlucks">
+            <Header />
+            <PotluckList />
+          </Route>
           <Route path="/signup">
+            <Header />
             <Signup />
           </Route>
           <Route path="/login">
+            <Header />
             <Login />
           </Route>
           <Route path="/">
+            <Header />
             <Login />
           </Route>
         </Switch>
---
+++ src/components/PotluckList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PotluckList() {
+  return <div></div>;
+}
+
+export default PotluckList;
