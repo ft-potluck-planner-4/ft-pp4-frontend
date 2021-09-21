bf2d223fb44dd17db02f126726c289198c4fe3ef
stevenjhomem <sjhomem@outlook.com>
2021-09-21 14:26:43 -0500

    only because git told me to

--- src/components/PotluckList.js
+++ src/components/PotluckList.js
@@ -8,7 +8,7 @@ import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-function PotluckList(props) {
+function PotluckList() {
 
   const { push } = useHistory();
   // console.log(push);
