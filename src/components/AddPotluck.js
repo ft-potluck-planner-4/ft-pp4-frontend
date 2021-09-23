34fce127278840d864202c33c8246de052ea7864
monicasalas86 <monicsalas167@gmail.com>
2021-09-23 02:12:21 -0500

    add potluck styling done

--- src/components/AddPotluck.js
+++ src/components/AddPotluck.js
@@ -7,51 +7,61 @@ export default function AddPotluck(){
         <div className='add-container'>
             <Form className='add-form'> 
                 <h2>Create a New Potluck</h2>
-                <FormGroup>
-                    <Label for='potluck_name'>Potluck Name</Label>
-                    <Input type='text' name='potluck_name' id='potluck_name' placeholder='potluck name'/>
+                <FormGroup row>
+                    <Label for='potluck_name' sm={2}>Potluck Name</Label>
+                    <Col sm={10}>
+                        <Input type='text' name='potluck_name' id='potluck_name' placeholder='potluck name'/>
+                    </Col>
                 </FormGroup>
-                <FormGroup>
-                    <Label for='location_name'>Name of Location</Label>
-                    <Input type='text' name='location_name' id='location_name' placeholder='name of location'/>
+                <FormGroup row>
+                    <Label for='location_name' sm={2}>Name of Location</Label>
+                    <Col sm={10}>
+                        <Input type='text' name='location_name' id='location_name' placeholder='name of location'/>
+                    </Col>
                 </FormGroup>  
-                <FormGroup>
-                    <Label for='location_address'>Address</Label>
-                    <Input type='text' name='location_address' id='location_address' placeholder='123 Main St'/>
-                </FormGroup>
-                <Row>
-                    <Col md={5}>
-                        <FormGroup>
-                            <Label for='location_city'>City</Label>
-                            <Input type='text' name='location_city' id='location_city' placeholder='city'/>
-                        </FormGroup>
+                <FormGroup row>
+                    <Label for='location_address' sm={2}>Address</Label>
+                    <Col sm={10}>
+                        <Input type='text' name='location_address' id='location_address' placeholder='123 Main St'/>
                     </Col>
-                    <Col md={4}>
-                        <FormGroup>
-                            <Label for='location_state'>State</Label>
-                            <Input type='text' name='location_state' id='location_state' placeholder='state' />
-                        </FormGroup>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for='location_city' sm={2}>City</Label>
+                    <Col sm={10}>
+                        <Input type='text' name='location_city' id='location_city' placeholder='city'/>
                     </Col>
-                    <Col md={2}>
-                        <FormGroup>
-                            <Label for='location_zip'>Zip Code</Label>
-                            <Input type='text' name='location_zip' id='location_zip' placeholder='zip code' />
-                        </FormGroup>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for='location_state' sm={2}>State</Label>
+                    <Col sm={10}>
+                        <Input type='text' name='location_state' id='location_state' placeholder='state' />
                     </Col>
-                </Row>
-                <FormGroup>
-                    <Label for='date'>Date</Label>
-                    <Input type='date' name='date' id='date' placeholder='date'/>
                 </FormGroup>
-                <FormGroup>
-                    <Label for='duration'>Duration</Label>
-                    <Input type='datetime' name='duration' id='duration' placeholder='3 Hours'/>
+                <FormGroup row>
+                    <Label for='location_zip' sm={2}>Zip Code</Label>
+                    <Col sm={10}>
+                        <Input type='text' name='location_zip' id='location_zip' placeholder='zip code' />
+                    </Col>
                 </FormGroup>
-                <FormGroup>
-                    <Label for='organizer'>Organizer</Label>
-                    <Input type='text' name='organizer' id='organizer' placeholder='organizer name'/>
+                <FormGroup row>
+                    <Label for='date' sm={2}>Date</Label>
+                    <Col sm={10}>
+                        <Input type='date' name='date' id='date' placeholder='date'/>
+                    </Col>
                 </FormGroup>
-                <FormGroup>
+                <FormGroup row>
+                    <Label for='duration' sm={2}>Duration</Label>
+                    <Col sm={10}>
+                        <Input type='datetime' name='duration' id='duration' placeholder='3 Hours'/>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for='organizer' sm={2}>Organizer</Label>
+                    <Col sm={10}>
+                        <Input type='text' name='organizer' id='organizer' placeholder='organizer name'/>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
                     <Col sm={{ size: 10, offset:1 }}>
                         <Button>Sumbit</Button>
                     </Col>
